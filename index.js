29661420ee5969430945a2f6c1be16ff9954679e
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-12-01 23:56:29 -0500

    added seeds and schema

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquier = require("inquirer");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+// creates connection to database
+const connection = mysql.createConnection(
+  {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  },
+  console.log("Welcome!")
+);
